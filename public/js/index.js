5c7cda3f9fbd1b547e127c102d2c188af6c6e219
Szymon <szymon.ludyga2@gmail.com>
2019-07-30 23:12:16 +0200

    Adding autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 const socket = io();
 
+function scrollToBottom() {
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -14,6 +32,7 @@ socket.on('newMessage', function(message) {
   li.text(`${message.from} ${message.createdAt}: ${message.text}`);
 
   jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.emit(
